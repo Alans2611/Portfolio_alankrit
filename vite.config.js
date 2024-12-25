c4cbd0a6797a6767f438a2679bb4af7cb6dc51d5
Alankrit <138154271+Alans2611@users.noreply.github.com>
2024-12-26 00:43:47 +0530

    deploy react to github

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Alankrit_portfolio"
+  base:"/Portfolio_alankrit/"
 })
